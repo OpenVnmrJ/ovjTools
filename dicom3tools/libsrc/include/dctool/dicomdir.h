292fdbdf8bd37cbe83e64016ef8731b664c7f2da
Dan Iverson <daniverson2016@gmail.com>
2017-07-27 17:57:04 -0700

    Revert "Added dicom3tools (#23)"
    This reverts commit 18b878e05b099d7bd91902ecc92ce1eef4b5a0d1.

--- dicom3tools/libsrc/include/dctool/dicomdir.h
+++
@@ -1,9 +0,0 @@
-#ifndef __Header_dicomdir__
-#define __Header_dicomdir__
-
-bool parseDicomdir(ManagedAttributeList &list,
-	TextOutputStream &log,bool verbose,bool veryverbose,bool showrecordinfo,bool showabstract,bool showpaths);
-
-#endif /* __Header_dicomdir__ */
-
-
--- dicom3tools/libsrc/include/dctool/sopcli.h
+++
@@ -1,6 +0,0 @@
-#ifndef __Header_sopcli__
-#define __Header_sopcli__
-
-extern const char *getDirectoryRecordTypeFromUID(const char *uid);
-
-#endif /* __Header_sopcli__ */
